fb9b9e5bcc60f570f575a28292435f01cdbfdad2
khadijahazward <91309302+khadijahazward@users.noreply.github.com>
2023-01-27 08:53:09 +0530

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['username']);
-unset($_SESSION['firstName']);
-unset($_SESSION['userID']);
-unset($_SESSION['role']);
-unset($_SESSION["logged_in"]);
-header("Location:login.php");
-?>
