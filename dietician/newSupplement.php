89d586ae5d85c7aebd808d84f36b8aa653c29a00
Kaveesha293 <2020is044@stu.ucsc.cmb.ac.lk>
2023-04-27 20:12:05 +0530

    updated

--- dietician/newSupplement.php
+++ dietician/newSupplement.php
@@ -21,84 +21,101 @@ include 'setProfilePic.php';
         } else {
             $supTypeErr = "Supplement Type is required";
         }
-
-        // $query2 = "SELECT * FROM supplementlist WHERE supplementName = $supName AND supplementType = $supType";
-        // $result2 = mysqli_query($conn, $query2);
-
-        // if(mysqli_num_rows($result2) > 0){
-        //     $supNameErr = "This supplement already exists in the list";
-        // }
         
-        if (isset($_FILES['image'])) {
-            
-            $errors = array();
-            $file_name = $_FILES['image']['name'];
-            $file_size = $_FILES['image']['size'];
-            $file_tmp = $_FILES['image']['tmp_name'];
-            $file_type = $_FILES['image']['type'];
+        $clearSupName = preg_replace('/[^A-Za-z0-9]/', '', strtolower($supName));
+        $clearSupType = preg_replace('/[^A-Za-z0-9]/', '', strtolower($supType));
 
-            $file_ext_array = explode('.', $file_name);
-            if (count($file_ext_array) > 1) {
-                $file_ext = strtolower(end($file_ext_array));
-            } else {
-                $errors[] = "File extension could not be determined.";
-            }
+        $query2 = "SELECT * FROM supplementlist";
+        $result2 = mysqli_query($conn, $query2);
 
-            $expensions = array("jpeg", "jpg", "png");
-            if (!in_array($file_ext, $expensions)) {
-                $errors[] = "Extension not allowed, please choose a JPEG or PNG file.";
-            }
-            if ($file_size > 2097152) {
-                $errors[] = 'File size must be exactly 2 MB';
-            }
+        if(mysqli_num_rows($result2) > 0){
+            while($row2 = mysqli_fetch_assoc($result2)){
+                
+                $clearDatabaseSupName = preg_replace('/[^A-Za-z0-9]/', '', strtolower($row2['supplementName']));
+                $clearDatabaseSupType = preg_replace('/[^A-Za-z0-9]/', '', strtolower($row2['supplementType']));
 
-            if (empty($errors)) {
-                // Get the user ID from the session
-                $userID = mysqli_real_escape_string($conn, $_SESSION['userID']);
-
-                $supNameFile = str_replace(' ', '', $supName);
-            
-                // Create a new file name using the user ID and the file extension
-                $newFileName = $supNameFile . '.' . $file_ext;
-            
-                // Set the folder where the uploaded file will be stored
-                $folder = "../supplement/";
-            
-                // Set the full path of the uploaded file
-                $file_path = $folder . $newFileName;
-            
-                // Move the uploaded file to the target directory
-                move_uploaded_file($file_tmp, $file_path);
-            
-                // Update the user's profile photo path in the database
-                $query = "INSERT INTO supplementlist(supplementName, supplementType, supplementPhoto) VALUES ('$supName', '$supType', '$file_path')";
-                $result = mysqli_query($conn, $query);
-            
-                if (!$result) {
-                    echo "Error: " . mysqli_error($conn);
-                } else {
-                    // Redirect the user to their profile page
-                    header("Location: dietplan.php");
-                    exit();
-                }
-            } else {
-                $query1 = "INSERT INTO supplementlist(supplementName, supplementType) VALUES ('$supName', '$supType')";
-                $result1 = mysqli_query($conn, $query1);
-
-                if (!$result1) {
-                    echo "Error: " . mysqli_error($conn);
-                } else {
-                    // Redirect the user to their profile page
-                    header("Location: dietplan.php");
-                    exit();
+                if(($clearDatabaseSupName = $clearSupName) && ($clearDatabaseSupType = $clearSupName)){
+                    $supNameErr = "This supplement is already existing in list";
                 }
             }
         }
-
         
-    }
+        if(empty($supNameErr) && empty($supTypeErr)){
+            
+            if (isset($_FILES['image'])) {
+            
+                $errors = array();
+                $file_name = $_FILES['image']['name'];
+                $file_size = $_FILES['image']['size'];
+                $file_tmp = $_FILES['image']['tmp_name'];
+                $file_type = $_FILES['image']['type'];
+
+                $file_ext_array = explode('.', $file_name);
+                if (count($file_ext_array) > 1) {
+                    $file_ext = strtolower(end($file_ext_array));
+                } else {
+                    $errors[] = "File extension could not be determined.";
+                }
+
+                $expensions = array("jpeg", "jpg", "png");
+                
+                if (!in_array($file_ext, $expensions)) {
+                    $errors[] = "Extension not allowed, please choose a JPEG or PNG file.";
+                }
+                
+                if ($file_size > 2097152) {
+                    $errors[] = 'File size must be exactly 2 MB';
+                }
+
+                if (empty($errors)) {
+                    // Get the user ID from the session
+                    $userID = mysqli_real_escape_string($conn, $_SESSION['userID']);
+
+                    $supNameFile = str_replace(' ', '', $supName);
+            
+                    // Create a new file name using the user ID and the file extension
+                    $newFileName = $supNameFile . '.' . $file_ext;
+            
+                    // Set the folder where the uploaded file will be stored
+                    $folder = "../supplement/";
+            
+                    // Set the full path of the uploaded file
+                    $file_path = $folder . $newFileName;
+            
+                    // Move the uploaded file to the target directory
+                    move_uploaded_file($file_tmp, $file_path);
+            
+                    // Update the user's profile photo path in the database
+                    $query = "INSERT INTO supplementlist(supplementName, supplementType, supplementPhoto) VALUES ('$supName', '$supType', '$file_path')";
+                    $result = mysqli_query($conn, $query);
+            
+                    if (!$result) {
+                        echo "Error: " . mysqli_error($conn);
+                    } else {
+                        // Redirect the user to their profile page
+                    header("Location: dietplan.php");
+                    exit();
+                    }
+                
+                } else {
+                    $query1 = "INSERT INTO supplementlist(supplementName, supplementType) VALUES ('$supName', '$supType')";
+                    $result1 = mysqli_query($conn, $query1);
+
+                    if (!$result1) {
+                        echo "Error: " . mysqli_error($conn);
+                    } else {
+                        // Redirect the user to their profile page
+                        header("Location: dietplan.php");
+                        exit();
+                    }
+                }
+            }
+
+        }
     
-    ?>
+    }
+
+?>
 
 
 
