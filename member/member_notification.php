ebf78ca82fb603f77b732e908cdb39d66acdeb8d
khadijahazward <khadijahazward@gmail.com>
2023-05-08 00:53:23 +0530

    Update member_notification.php

--- member/member_notification.php
+++ member/member_notification.php
@@ -50,3 +50,52 @@
     }
 
 ?>
+
+<?php
+
+//for DIET SERVICE
+    $userID = $_SESSION['userID'];
+
+   $query = "SELECT * FROM serviceCharge WHERE memberID = $row1[memberID] AND serviceID = 3 AND endDate <= NOW() ORDER BY endDate DESC LIMIT 1";
+   $result = mysqli_query($conn, $query);
+
+   if ($result && mysqli_num_rows($result) > 0) {
+       $row = mysqli_fetch_assoc($result);
+       $endDate = date('Y-m-d', strtotime($row['endDate'])); // Retrieves only the date part of the endDate field 
+       $serviceID = $row['serviceID'];
+       
+       $service = "gym";
+
+        if($serviceID == 3){
+            $service = "Diet"; 
+        }
+       
+       // for type: Announcement = 0, Complaint = 1, Payment = 2, service = 3, appointment = 4
+       $sql6 = "SELECT notificationsID FROM notifications WHERE type = 3 AND DATE(created_at) = '$endDate' AND notificationsID IN (SELECT notificationsID FROM usernotifications WHERE userID = $userID)";
+       $result6 = mysqli_query($conn, $sql6);
+
+       if ($result6 && mysqli_num_rows($result6) > 0) {
+           // Notification has already been sent
+       } else {
+           // Insert a new record into the notifications table
+           $message = "Congratulations on completing the $service service! We hope that it was a valuable experience for you and we look forward to serving you again in the future.";
+           $type = 3;
+           $created_at = $row['endDate'];
+
+           $sql7 = "INSERT INTO notifications(message, type, created_at) VALUES ('$message', '$type', '$created_at')";
+           $result7 = mysqli_query($conn, $sql7);
+
+           if ($result7) {
+               //getting previous inserted notification id
+               $notificationID = mysqli_insert_id($conn);
+
+               // Insert a new record into the user_notifications table 
+               $sql8 = "INSERT INTO usernotifications(userID, notificationsID) VALUES ('$userID', '$notificationID')";
+               $result8 = mysqli_query($conn, $sql8);
+           }else{
+               echo "Error: " . mysqli_error($conn);
+           }
+       }
+    }
+
+?>
