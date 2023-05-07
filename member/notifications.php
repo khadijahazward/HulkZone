5911096f5036877e54db00ffa2df265d623f18b1
khadijahazward <khadijahazward@gmail.com>
2023-05-07 23:52:18 +0530

    update

--- member/notifications.php
+++ member/notifications.php
@@ -1,12 +1,53 @@
 <!DOCTYPE html>
-<head></head>
+<head>
+    <style>
+        .bell-icon {
+            position: relative;
+            padding: 0;
+            margin: 0;
+            background-color: transparent;
+            border: none;
+            box-shadow: none;
+        }
+        .notification-count {
+            position: absolute;
+            top: -5px;
+            right: -5px;
+            background-color: red;
+            color: white;
+            font-size: 12px;
+            width: 15px;
+            height: 15px;
+            text-align: center;
+            border-radius: 50%;
+        }
+    </style>
+</head>
 <body>
+    <?php
+        $userID = $_SESSION['userID'];
+
+        //for displaying number of notifications
+        $sql14 = "SELECT COUNT(*) AS unread_count FROM usernotifications WHERE userID = $userID AND status = 0";
+        $result14 = mysqli_query($conn, $sql14);
+        if ($result14) {
+            $row14 = mysqli_fetch_assoc($result14);
+            $unread_count = $row14['unread_count'];
+        } else {
+            $unread_count = 0;
+        }
+    ?>
     <button class="bell-icon" onclick="toggleNotificationPopup()">
         <img src="..\asset\images\bell.png" alt="notification" width="35px" height="35px">
+        <?php
+            if($unread_count > 0){
+                echo "<span class='notification-count'>" . $unread_count . "</span>";
+            }
+        ?>
     </button>
     <div class="notification-popup" id="notificationPopup">
         <?php 
-            $userID = $_SESSION['userID'];
+           
             $sql13 = "SELECT n.message, n.created_at, un.usernotificationsID FROM notifications n JOIN usernotifications un ON n.notificationsID=un.notificationsID WHERE un.userID = $userID AND un.status = 0";
             $result13= mysqli_query($conn, $sql13);
             $notifications = '';
