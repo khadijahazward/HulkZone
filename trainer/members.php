a0df75423a5bddc0d69dd51b41be2cf971268024
Eshitha09 <eshithasuradin123@gmail.com>
2023-05-15 00:37:23 +0530

    dashboard style

--- trainer/members.php
+++ trainer/members.php
@@ -12,6 +12,7 @@ include 'script/config.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/style.css">
+    <a href="search-box.php">SearchBar</a>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
     <title>Hulkzone</title>
 </head>
